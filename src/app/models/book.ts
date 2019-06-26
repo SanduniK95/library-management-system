d68170e00f3325c288c00306ca25dcc2cc8d8a3e
dimuthi <32828462+dimuthi@users.noreply.github.com>
2019-06-26 14:09:09 +0530

    book model

---
+++ src/app/models/book.ts
@@ -0,0 +1,9 @@
+export class Book {
+    constructor(
+        public _id:string,
+        public bookName:string,
+        public author:string, 
+        public isbn:string, 
+        public type:string){
+    }
+}
